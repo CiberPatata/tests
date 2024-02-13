cfc2074b0db24f9f1c0990dd5b23b5b289636acb
Ciberpatata <128803668+CiberPatata@users.noreply.github.com>
2024-02-13 12:33:49 +0100

    Update ADV-Recon.ps1

--- ADV-Recon.ps1
+++ ADV-Recon.ps1
@@ -1,11 +1,18 @@
-# Define el idioma que deseas establecer (por ejemplo, "es-ES" para español de España)
-$desiredLanguage = "es-ES"
+# Define el idioma y la distribución del teclado que deseas establecer (por ejemplo, "en-US" para inglés de Estados Unidos y "0409:00000409" para la distribución del teclado en inglés)
+$desiredLanguage = "en-US"
+$desiredKeyboardLayout = "0409:00000409"
 
-# Agrega el idioma especificado
-Add-WindowsLanguage -Language $desiredLanguage
+# Instala el paquete de idioma inglés (Estados Unidos)
+Install-WindowsPackage -Online -PackageName Microsoft-Windows-Client-Language-Pack-Package~31bf3856ad364e35~amd64~$desiredLanguage~10.0.19041.1
 
 # Establece el idioma recién agregado como idioma de visualización predeterminado
 Set-WinUILanguageOverride -Language $desiredLanguage
+
+# Establece la distribución del teclado especificada
+Set-WinUserLanguageList -LanguageList $desiredLanguage -Force
+
+# Configura la distribución del teclado
+Set-WinUserLanguageList -LanguageList $desiredKeyboardLayout -Force
 
 # Reinicia el sistema para aplicar los cambios
 Restart-Computer -Force
