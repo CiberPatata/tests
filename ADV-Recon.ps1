ccaa46bcbfaf68301fe6189a16a0e70b198cdac0
Ciberpatata <128803668+CiberPatata@users.noreply.github.com>
2024-02-15 08:22:22 +0100

    Update ADV-Recon.ps1

--- ADV-Recon.ps1
+++ ADV-Recon.ps1
@@ -1,11 +1,11 @@
-# Define el idioma que deseas establecer (en este caso, inglés de Estados Unidos)
-$desiredLanguage = "en-US"
+# Define el idioma que deseas establecer (en este caso, español de España)
+$desiredLanguage = "es-ES"
 
-# Define la distribución del teclado que deseas establecer (en este caso, inglés de Estados Unidos)
-$desiredKeyboardLayout = "0409:00000409"
+# Define la distribución del teclado que deseas establecer (en este caso, español de España)
+$desiredKeyboardLayout = "040A:0000040A"
 
 # Agrega el idioma especificado
-Add-WindowsPackage -Online -PackagePath "C:\Path\To\LanguagePack\lp.cab" -NoRestart
+Add-WindowsPackage -Online -PackagePath "C:\Ruta\Al\PaqueteDeIdioma\lp.cab" -NoRestart
 
 # Establece el idioma recién agregado como idioma de visualización predeterminado
 Set-WinSystemLocale -SystemLocale $desiredLanguage -Force
@@ -21,3 +21,4 @@ Set-WinUserLanguageList -LanguageList $desiredLanguage -KeyboardList $desiredKey
 
 # Reinicia el sistema para aplicar los cambios
 Restart-Computer -Force
+
